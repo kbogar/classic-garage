a6899a0c5cb4762e41f057a2117c7c890d0c861b
kbogar <krisztianbogar@gmail.com>
2023-04-09 09:29:06 +0000

    Create the first ERD diagram and start the booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+STATUS = ((0, 'Pending'), (1, 'Approved'))
+
+
+class BookingModel(models.Model):
+    """
+    This defines the Booking Model
+    """
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='booking')
