b3ddee8f5ae09e9a7b00506cbc287bdd26cd96ac
kbogar <krisztianbogar@gmail.com>
2023-04-11 10:12:12 +0000

    create Custom Model and migrate to database

--- booking/models.py
+++ booking/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from services.models import Service
 
+
 STATUS = ((0, 'Pending'), (1, 'Approved'))
 
 
 class BookingModel(models.Model):
     """
-    This defines the Booking Model, the fielad are needed
+    This defines the Booking Model, the field are needed
     for the customer to make a booking.
     """
-    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='booking')
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='booking')
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=40)
     email = models.EmailField()
     created_on = models.DateField()
     updated_on = models.DateTimeField(auto_now=True)
@@ -22,6 +24,22 @@ class BookingModel(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+    def __str__(self):
+        return str(self.name)
+
+
+class CustomModel(models.Model):
+    """
+    This defines the Custom Model, with contact details
+    form.
+    """
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    message = models.TextField(max_length=400, blank=True)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return str(self.name)
