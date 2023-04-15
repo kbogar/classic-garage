1551034a94dd83af8ff3df04605232bb55cd491b
kbogar <krisztianbogar@gmail.com>
2023-04-15 00:06:55 +0000

    add views and urls path

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from .views import Home, About, OurServices, BookService, Contact
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='base.html'))
+    path('', Home.as_view(), name='home'),
+    path('about/', About.as_view(), name='about'),
+    path('our_services/', OurServices.as_view(), name='our_services'),
+    path('book_service/', BookService.as_view(), name='book_service'),
+    path('contact/', Contact.as_view(), name='contact'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -2,5 +2,21 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-class template_test_view(TemplateView):
-    template_name = 'base.html'
+class Home(TemplateView):
+    template_name = 'home.html'
+
+
+class About(TemplateView):
+    template_name = 'about.html'
+
+
+class OurServices(TemplateView):
+    template_name = 'our_services.html'
+
+
+class BookService(TemplateView):
+    template_name = 'book_service.html'
+
+
+class Contact(TemplateView):
+    template_name = 'contact.html'
